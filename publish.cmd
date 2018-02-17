38416c113e22e899ff6595684dbfaa14e7a3ff45
Javier Ignacio Quinteros Carvajal <javier.quinteros@inacapmail.cl>
2018-02-16 22:43:10 -0300

    Primer Commit (Preparando la implementación continua)

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.ProactiveBot.sln -p:DeployOnBuild=true -p:PublishProfile=tanukibot-Web-Deploy.pubxml -p:Password=YwWGvgkT8k8J7qj8e6i6Jryzo4znS9Mw185u5nBzok1pqa8f7A67dpwCr2MX
+
